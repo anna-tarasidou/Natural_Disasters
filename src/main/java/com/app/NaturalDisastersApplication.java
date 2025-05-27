afffa3c6a2b0aae60e90296f5f3fdec049ea2770
Anna Tarasidou <tarasidouu@gmail.com>
2025-05-27 17:48:55 +0300

    Update classes

--- src/main/java/com/app/NaturalDisastersApplication.java
+++ src/main/java/com/app/NaturalDisastersApplication.java
@@ -1,8 +1,5 @@
 package com.app;
 
-import java.awt.Desktop;
-import java.net.URI;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +8,5 @@ public class NaturalDisastersApplication {
 
 	public static void main(String[] args) {
 	    SpringApplication.run(NaturalDisastersApplication.class, args);
-
-	    try {
-	        if (Desktop.isDesktopSupported()) {
-	            Desktop.getDesktop().browse(new URI("http://localhost:8080/disasters/"));
-	        }
-	    } catch (Exception e) {
-	        System.err.println("Failed to open browser: " + e.getMessage());
-	    }
 	}
 }
